620fe3de37e7f926a9e90b5e5ccf1680bce36181
March.Anastasiia <nastya.marchenko00@gmail.com>
2025-09-27 23:26:29 +0300

    сделано меню для 1160

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 let burger = document.querySelector(".header_burger__mobile");
 let menuMobile = document.querySelector(".menu_mobile");
-let headerLogoMobile = document.querySelector(".header_logo__mobile");
-let headerMobile = document.querySelector(".header_mobile");
+let headerContainer = document.querySelector(".header_container");
+let header = document.querySelector(".header");
 let cover = document.querySelector(".cover");
 
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
     menuMobile.classList.toggle("active");
-    headerLogoMobile.classList.toggle("active");
-    headerMobile.classList.toggle("active");
+    headerContainer.classList.toggle("active");
+    header.classList.toggle("active");
     cover.classList.toggle("active");
 });
 
 cover.addEventListener("click", () => {
     burger.classList.remove("active");
     menuMobile.classList.remove("active");
-    headerLogoMobile.classList.remove("active");
-    headerMobile.classList.remove("active");
+    headerContainer.classList.remove("active");
+    header.classList.remove("active");
     cover.classList.remove("active");
 });
+
