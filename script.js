b366945b0d81d1d2f1530e4c89b0803f8cf4a182
March.Anastasiia <nastya.marchenko00@gmail.com>
2025-09-27 18:11:26 +0300

    изменена вёрстка

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let burger = document.querySelector(".header_burger__mobile");
 let menuMobile = document.querySelector(".menu_mobile");
+let headerLogoMobile = document.querySelector(".header_logo__mobile");
+let headerMobile = document.querySelector(".header_mobile");
 let cover = document.querySelector(".cover");
 
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
     menuMobile.classList.toggle("active");
+    headerLogoMobile.classList.toggle("active");
+    headerMobile.classList.toggle("active");
     cover.classList.toggle("active");
 });
 
 cover.addEventListener("click", () => {
     burger.classList.remove("active");
     menuMobile.classList.remove("active");
+    headerLogoMobile.classList.remove("active");
+    headerMobile.classList.remove("active");
     cover.classList.remove("active");
 });
